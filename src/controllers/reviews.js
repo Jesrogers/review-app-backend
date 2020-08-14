fb53cf18662a8ad179d8208b8fb2875854dd4f42
Jesse <jesse.rogers@uconn.edu>
2020-08-14 19:56:53 -0400

    get endpoints

--- src/controllers/reviews.js
+++ src/controllers/reviews.js
@@ -1,1 +1,28 @@
-const getReviews = (req, res) => {};
+const db = require('../db');
+
+const getReviews = async (req, res) => {
+  try {
+    const reviews = await db.query('SELECT * FROM review');
+    res.status(200).json(reviews.rows);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const getReview = async (req, res) => {
+  const reviewId = req.params.id;
+
+  try {
+    const review = await db.query('SELECT * FROM review WHERE id = $1', [
+      reviewId,
+    ]);
+    res.status(200).json(review.rows);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = {
+  getReviews,
+  getReview,
+};
--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,7 +1,7 @@
 const reviewsRouter = require('express').Router();
+const { getReviews, getReview } = require('../controllers/reviews');
 
-reviewsRouter.get('/', (req, res) => {
-  res.json('Get review endpoint');
-});
+reviewsRouter.get('/', getReviews);
+reviewsRouter.get('/:id', getReview);
 
 module.exports = reviewsRouter;
