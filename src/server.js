1b4d13b37857d242713c09200f891337eabf443a
Jesse <jesse.rogers@uconn.edu>
2020-09-03 13:44:16 -0400

    changed default port

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use('/api/reviews', reviewsRouter);
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(
   PORT,
   logger.info(`Review app listening at http://localhost:${PORT}`)
